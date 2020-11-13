38cc141092df641d617fb0b04ccaca4e9475ea38
Yadiel <ymglez2015@gmail.com>
2020-11-13 00:56:38 -0500

    test key-value store packages FASTER and LightningDB (wins)

---
+++ src/backend/GoogleForADay.Core/Model/WebSite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleForADay.Core.Model
+{
+    public class WebSite : Entity
+    {
+        public string Url { get; set; }
+
+        public int WordsFounded { get; set; }
+
+        public DateTime LastAccess { get; set; }
+    }
+}
---
+++ src/backend/GoogleForADay.Tests/BaseTest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using GoogleForADay.Core.Abstractions.Store;
+using GoogleForADay.Core.Model;
+using GoogleForADay.Infrastructure.Store.FASTER;
+using GoogleForADay.Infrastructure.Store.LightningDB;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace GoogleForADay.Tests
+{
+    public class BaseTest
+    {
+        protected IServiceProvider ServiceProvider;
+
+        public BaseTest()
+        {
+            ServiceProvider = new ServiceCollection()
+                .AddSingleton<IKeyValueRepository<Keyword>, LightningKeywordRepository>()
+                .BuildServiceProvider();
+        }
+
+        
+    }
+}
