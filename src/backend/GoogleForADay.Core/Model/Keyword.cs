1944083dc34176171c28a7e6b7f5bb545780d15f
Yadiel <ymglez2015@gmail.com>
2020-11-13 00:53:22 -0500

    Declaring abstractions in core proyect

---
+++ src/backend/GoogleForADay.Core/Model/Keyword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleForADay.Core.Model
+{
+    public class Keyword : Entity
+    {
+        public string Term { get; set; }
+
+        public List<Reference> References { get; set; }
+    }
+}
