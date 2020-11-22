42475c58d735119c00b29b0784617ac8a3f83c57
Yadiel <ymglez2015@gmail.com>
2020-11-22 14:04:13 -0500

    search component

---
+++ src/frontend/src/app/components/search/search.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SearchResults, SearchStateEnum, SearchQuery } from '@app/app.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { Observable, of, concat, Subject } from 'rxjs';
+import { map, switchMap, filter, catchError, takeUntil } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { SearchService } from '@app/services/search.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit, OnDestroy {
+
+  destroy$ = new Subject<void>();
+
+  control = new FormControl();
+  query$: Observable<SearchQuery>;
+
+  constructor(private httpClient: HttpClient, private searchService: SearchService) {
+
+  }
+
+  ngOnInit(): void {
+    this.query$ = this.searchService.onSearchSubmit()
+      .pipe(
+        filter(query => !!query.term),
+        switchMap(query =>
+          concat(
+            of({ state: SearchStateEnum.Loading } as SearchQuery),
+            this.doSearch(query.term).pipe(
+              map(results => <SearchQuery>{state: SearchStateEnum.Finished, data: results}),
+              catchError(err => of(<SearchQuery>{ state: SearchStateEnum.Error, error: err }))
+            )
+          )
+        ),
+        takeUntil(this.destroy$)
+      );
+  }
+
+  doSearch(query: string): Observable<SearchResults> {
+    return this.httpClient
+      .get<SearchResults>(`${environment.apiUrl}/search`, {
+        params: {
+          q: query
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
