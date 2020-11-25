e2fbf4ee96cfe8865b177a33a67779878a2b56e0
Yadiel <ymglez2015@gmail.com>
2020-11-24 23:20:18 -0500

    switch to alpine image instead stretch

--- src/build.sh
+++ src/build.sh
@@ -7,5 +7,9 @@ echo 'EXEC => dotnet publish -c Release -o ../../out backend/GoogleForADay.Servi
 dotnet publish -c Release -o ../../out backend/GoogleForADay.Services.Api/
 
 echo 'EXEC => npm run env -s && npm run build -- --prod'
-cd frontend
-npm run env -s && npm run build -- --prod
+  
+ #BASH
+ docker run --name dotnet-app-container -d -p 44340:80 -p 44341:443 -e ASPNETCORE_URLS="https://+:443;http://+:80" -e ASPNETCORE_HTTPS_PORT=44341 -e ASPNETCORE_Kestrel__Certificates__Default__Password=password -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx -v %USERPROFILE%/.aspnet/https:/https:ro googleforaday-backend
+
+ # CMD
+ docker run --name dotnet-app-alpine-container -d -p 44340:80 -p 44341:443 -e ASPNETCORE_URLS="https://+:443;http://+:80" -e ASPNETCORE_HTTPS_PORT=44341 -e ASPNETCORE_Kestrel__Certificates__Default__Password=password -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx -v %USERPROFILE%/.aspnet/https:/https:ro googleforaday-backend-alpine 
